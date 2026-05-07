6b93151e9f874e6fecc1b59d7faaefe783f992f6
Alper <alperisbeast@gmail.com>
2026-05-08 01:01:26 +1200

    Minmize & interval handlers

--- preload.js
+++ preload.js
@@ -3,6 +3,12 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
     getTasks: () => ipcRenderer.invoke('get-tasks'),
     saveTasks: (tasks) => ipcRenderer.invoke('save-tasks', tasks),
+    getInterval: () => ipcRenderer.invoke('get-interval'),
+    saveInterval: (min) => {
+        ipcRenderer.send('save-setting', {key: 'resetInterval', value: min})
+        ipcRenderer.send('update-timer') // main.js restarts clock
+    },
     changePage: (pageName) => ipcRenderer.send('change-page', pageName),
+    minimizeApp: () => ipcRenderer.send('minimize-app'),
     quitApp: () => ipcRenderer.send('quit-app'),
 });

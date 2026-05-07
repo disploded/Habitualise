04bd9ddb34ced2e548e263eb99cdd3883b342dac
Alper <alperisbeast@gmail.com>
2026-05-07 19:45:12 +1200

    Contextbridge for events

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
-    getTasks: () => ipcRenderer.invoke('get-tasks')
+    getTasks: () => ipcRenderer.invoke('get-tasks'),
+    saveTasks: (tasks) => ipcRenderer.invoke('save-tasks', tasks),
+    changePage: (pageName) => ipcRenderer.send('change-page', pageName),
+    quitApp: () => ipcRenderer.send('quit-app'),
 });
